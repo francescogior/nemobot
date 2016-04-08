4978462860b9919e528dea3cfcdde2c6958e5c2a
Francesco <firefelix@gmail.com>
2016-04-08 17:11:32 +0200

    return 403 if event is not from an active platform

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import Rx from 'rx';
+import { find } from 'lodash';
 import processors from './processors';
 import { isEvent } from './validators';
+import config from './config';
 
+const platforms = config.platforms.map(p => `x-${p}-event`);
 const subject = new Rx.Subject();
 
 const onNext = (event, delay) => {
@@ -18,11 +21,14 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/', ({ body, headers }, res) => {
-  const event = headers['x-github-event'];
+  const event = headers[find(platforms, p => headers[p])];
 
-  onNext({ event, body });
-
-  res.send('👍');
+  if (event) {
+    onNext({ event, body });
+    res.send('👍');
+  } else {
+    res.status(403).send('Forbidden');
+  }
 });
 
 app.listen(3000);
